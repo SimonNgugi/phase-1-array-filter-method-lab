089a139ffa10244d7626db633bdcf084ce428030
Simon Ngugi Ngigi <simonngugi88@gmail.com>
2022-08-25 21:48:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching (drivers,string){
+    return drivers.filter(drivers => drivers.toUpperCase() === string.toUpperCase())
+}
+
+function fuzzyMatch (drivers,string){
+    let querry = string.length;
+    return drivers.filter(drivers => drivers.slice(0, querry) === string)
+ }
+
+ function matchName (driver,strng){
+     return driver.filter(driver => driver.name === strng)
+}
